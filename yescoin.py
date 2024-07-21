044f6f5bff6fa875d215f04d61f1061806b5b859
tokecajah <tokec65@gmail.com>
2024-07-22 01:09:52 +0700

    Update yescoin.py

--- yescoin.py
+++ yescoin.py
@@ -357,13 +357,13 @@ def process_account(token):
         squad_members = squad_info['data']['squadInfo']['squadMembers']
         print(f"{random.choice(available_colors)+Style.BRIGHT}\r[ Squad ] : {squad_title} | {squad_members} Members")
     else:
-        print(f"{Fore.YELLOW + Style.BRIGHT}\r[ Squad ] : Belum Join Squad. Joining Ghalibie.", end="", flush=True)
+        print(f"{Fore.YELLOW + Style.BRIGHT}\r[ Squad ] : Belum Join Squad. Joining saderekderin.", end="", flush=True)
         time.sleep(3)
-        join_result = join_squad(token, "t.me/ghalibie")
+        join_result = join_squad(token, "t.me/saderekderin")
         if join_result:
-            print(f"{random.choice(available_colors) + Style.BRIGHT}\r[ Squad ] : Welcome Pemulung {nickname} - Ghalibie !      ", flush=True)
+            print(f"{random.choice(available_colors) + Style.BRIGHT}\r[ Squad ] : Welcome Pemulung {nickname} - saderekderin !      ", flush=True)
         else:
-            print(f"{random.choice(available_colors) + Style.BRIGHT}\r[ Squad ] : Failed to join Ghalibie.", flush=True)
+            print(f"{random.choice(available_colors) + Style.BRIGHT}\r[ Squad ] : Failed to join saderekderin.", flush=True)
 
     print(f"{random.choice(available_colors)+Style.BRIGHT}\r[ Balance ] : Getting...", end="", flush=True)
     balance = fetch_account_info(token)
